755ed7a8c2942ba7bcbab51d8cb3177f99c21e90
Noozh <sirl.daniel.arevalo@hotmail.com>
2019-11-13 12:44:00 +0100

    Corregidos los casos en los que no se lanza la excepcion

--- Practica_IDM/src/E1_program/Bisiesto.java
+++ Practica_IDM/src/E1_program/Bisiesto.java
@@ -5,7 +5,7 @@ import java.security.InvalidParameterException;
 public class Bisiesto{
 
 	public static boolean esBisiesto(int ano) throws InvalidParameterException {
-		if(ano != 0) {
+		if(ano <= 0) {
 			if ((ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0)))
 				return true;
 			else
