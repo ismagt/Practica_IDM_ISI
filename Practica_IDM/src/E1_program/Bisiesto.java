d4c60a1f25b7bf16022e34f80658808e940d159a
Noozh <sirl.daniel.arevalo@hotmail.com>
2019-11-13 12:01:17 +0100

    Metodo esBisiesto creado

--- Practica_IDM/src/E1_program/Bisiesto.java
+++ Practica_IDM/src/E1_program/Bisiesto.java
@@ -1,6 +1,16 @@
 package E1_program;
 
+import java.security.InvalidParameterException;
+
 public class Bisiesto {
+
+		public boolean esBisiesto(int ano) throws InvalidParameterException {
+			if ((ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0)))
+				return true;
+			else
+				return false;
+		
+		}
 	
 
 }
