29700eb834fdf12fbb155dba6cb6930262a3f7e1
Noozh <sirl.daniel.arevalo@hotmail.com>
2019-11-13 12:35:04 +0100

    Lanzada excepcion IvalidParameter cuando el año es 0

--- Practica_IDM/src/E1_program/Bisiesto.java
+++ Practica_IDM/src/E1_program/Bisiesto.java
@@ -5,9 +5,12 @@ import java.security.InvalidParameterException;
 public class Bisiesto{
 
 	public static boolean esBisiesto(int ano) throws InvalidParameterException {
-		if ((ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0)))
-			return true;
-		else
-			return false;
+		if(ano != 0) {
+			if ((ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0)))
+				return true;
+			else
+				return false;
+		}else
+			throw new  InvalidParameterException();
 	}
 }
