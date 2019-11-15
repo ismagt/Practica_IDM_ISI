64762466f8832d3ab9ddead145c0143b5932a2df
ISMAEL <isma98.igm@gmail.com>
2019-11-15 09:39:03 +0100

    mejorando test de descuento de black fryday

--- Practica_IDM/src/E4_test/DescuentoBlackFridayTest.java
+++ Practica_IDM/src/E4_test/DescuentoBlackFridayTest.java
@@ -1,5 +1,7 @@
 package E4_test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.security.InvalidParameterException;
 
 
@@ -12,6 +14,8 @@ public class DescuentoBlackFridayTest {
 	
 	private double precioOriginal;
 	private double descuento;
+	private double precioComparar;
+	private double myPi = 22.0d / 7.0d;
 
 	@Before      // Set up - Called before every test method.
 	public void setUp(){
@@ -29,56 +33,57 @@ public class DescuentoBlackFridayTest {
 	public void testAlgorithm() {
 		precioOriginal = 100;
 		descuento = 2;
-		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+		precioComparar = 98.0;
+		assertEquals(precioComparar, myPi, DescuentoBlackFriday.precioFinal(precioOriginal, descuento));
 	}
 	
 	@Test
 	public void test1() {
-		precioOriginal = 0.00000000001;
+		precioOriginal = 0.0001;
 		descuento = -3;
-		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+		assertEquals(InvalidParameterException.class, DescuentoBlackFriday.precioFinal(precioOriginal, descuento));
 	}
 	
 	@Test
 	public void test2() {
 		precioOriginal = 0.00000000001;
 		descuento = -3000;
-		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+		assertEquals(InvalidParameterException.class, DescuentoBlackFriday.precioFinal(precioOriginal, descuento));
 	}
 	
 	@Test
 	public void test3() {
 		precioOriginal = -1000000;
 		descuento = 3000;
-		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+		assertEquals(InvalidParameterException.class, DescuentoBlackFriday.precioFinal(precioOriginal, descuento));
 	}
 	
 	@Test
 	public void test4() {
 		precioOriginal = -5;
 		descuento = 0.33333333333333333;
-		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+		assertEquals(InvalidParameterException.class, DescuentoBlackFriday.precioFinal(precioOriginal, descuento));
 	}
 	
 	@Test
 	public void test5() {
 		precioOriginal = -5;
 		descuento = 1;
-		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+		assertEquals(InvalidParameterException.class, DescuentoBlackFriday.precioFinal(precioOriginal, descuento));
 	}
 	
 	@Test
 	public void test6() {
 		precioOriginal = 0.1;
 		descuento = 200;
-		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+		assertEquals(InvalidParameterException.class, DescuentoBlackFriday.precioFinal(precioOriginal, descuento));
 	}
 	
 	@Test
 	public void test7() {
 		precioOriginal = 0.1;
 		descuento = 150;
-		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+		assertEquals(InvalidParameterException.class, DescuentoBlackFriday.precioFinal(precioOriginal, descuento));
 	}
 	
 
