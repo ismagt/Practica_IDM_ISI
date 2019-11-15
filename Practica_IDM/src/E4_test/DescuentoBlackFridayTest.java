17fb9d6770fb2b35df185446a12f0812f1a3c80b
ISMAEL <isma98.igm@gmail.com>
2019-11-15 08:49:24 +0100

    añadiendo algunos test mas happy

--- Practica_IDM/src/E4_test/DescuentoBlackFridayTest.java
+++ Practica_IDM/src/E4_test/DescuentoBlackFridayTest.java
@@ -1,5 +1,8 @@
 package E4_test;
 
+import java.security.InvalidParameterException;
+
+
 import org.junit.*;
 
 import E3_program.Embotelladora;
@@ -24,8 +27,57 @@ public class DescuentoBlackFridayTest {
 	
 	@Test
 	public void testAlgorithm() {
-		precioOriginal = 1.0;
-		descuento = 10.0;
+		precioOriginal = 100;
+		descuento = 2;
+		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+	}
+	
+	@Test
+	public void test1() {
+		precioOriginal = 0.00000000001;
+		descuento = -3;
+		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+	}
+	
+	@Test
+	public void test2() {
+		precioOriginal = 0.00000000001;
+		descuento = -3000;
+		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+	}
+	
+	@Test
+	public void test3() {
+		precioOriginal = -1000000;
+		descuento = 3000;
+		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+	}
+	
+	@Test
+	public void test4() {
+		precioOriginal = -5;
+		descuento = 0.33333333333333333;
+		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+	}
+	
+	@Test
+	public void test5() {
+		precioOriginal = -5;
+		descuento = 1;
+		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+	}
+	
+	@Test
+	public void test6() {
+		precioOriginal = 0.1;
+		descuento = 200;
+		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
+	}
+	
+	@Test
+	public void test7() {
+		precioOriginal = 0.1;
+		descuento = 150;
 		DescuentoBlackFriday.precioFinal(precioOriginal, descuento);
 	}
 	
